92c4eb787a192ba89e5dfa5d1612a149ef724cf2
Dewliak <veres.z.benedek@gmail.com>
2025-09-14 22:31:37 +0200

    Added feature On the home page create tables + TITLE Fixes #10 [GUI] - Create a page for adding books Fixes #3 [GUI] - create a book returnal page Fixes #4

--- gui/pages/add_book_page.py
+++ gui/pages/add_book_page.py
@@ -1,8 +1,45 @@
 from nicegui import ui
 from gui.components import header
+from gui.services import DataClientSingleton
+from lmsapi import DataClient
 
+
+loading_spinner = None
+
+def submit_form(client: DataClient,title: str, author: str, isbn: str, quantity: int):
+    global loading_spinner
+    try:
+        loading_spinner.set_visibility(True)
+        client.add_book(title=title, author=author, isbn=isbn, quantity=quantity)
+        ui.notify("Book added successfully", type='positive')
+    except:
+        ui.notify("There was an error when adding the book", type='negative')
+    finally:
+        loading_spinner.set_visibility(False)
 
 @ui.page("/add_book")
 def add_book_page():
+    global loading_spinner
     header.create_header()
-    ui.label("Hello")
+    client = DataClientSingleton.get_instance()
+
+    with ui.row().classes('w-full h-screen flex justify-center pt-10'):
+        with ui.column().classes('border border-gray-300 rounded-xl p-6 bg-white gap-4 items-center'):
+            #title
+            title_input = ui.input(placeholder="Title").classes('w-96').props('w-200 rounded outlined dense')
+            #author
+            author_input = ui.input(placeholder="Author").classes('w-96').props('rounded outlined dense')
+            #ISBN
+            isbn_input = ui.input(placeholder="ISBN").classes('w-96').props('rounded outlined dense')
+            #quantity
+            quantity_input = ui.number(label='Quantity',value=1,min=1).classes('text-right w-96').props('rounded outlined dense')
+
+            with ui.row():
+
+                submit_button = ui.button(text="Submit", on_click=lambda _: submit_form(client,
+                                                                                    title_input.value,
+                                                                                    author_input.value,
+                                                                                    isbn_input.value,
+                                                                                    quantity_input.value))
+                loading_spinner = ui.spinner(size='lg')
+                loading_spinner.set_visibility(False)
--- gui/pages/main_page.py
+++ gui/pages/main_page.py
@@ -1,8 +1,76 @@
 from nicegui import ui
 from gui.components import header
 
+from datetime import date, datetime
+from lmsapi import DataClient, SheetName, Book
+from gui.services import DataClientSingleton
 
 @ui.page("/")
 def main_page():
     header.create_header()
-    ui.label("Hello")
+
+    
+
+    client = DataClientSingleton.get_instance()
+
+    # get all distinct books
+    # get books amount
+    # get books due
+
+    
+    book_df = client.get_sheet(SheetName.BOOK)
+    lend_df = client.get_sheet(SheetName.LEND)
+    print()
+    print()
+    #print(lend_df[(datetime.strptime(lend_df['END_DATE'], "%d/%m/Y") <= date.today()) ]  )
+    late_lends = []
+    for index,row in lend_df.iterrows():
+        if datetime.strptime(row['END_DATE'], "%d/%m/%Y") <= datetime.now():
+            late_lends.append(row.to_dict())
+    
+    print(late_lends)
+    
+
+ 
+    with ui.row().classes('w-full h-screen flex-nowrap'):
+        # LEFT HALF
+        with ui.column().classes(
+            'w-1/2 h-full flex justify-start items-center bg-gray-100 pt-[10vh]'
+        ):
+            with ui.row().classes(
+                'bg-white rounded-2xl shadow-xl aspect-square w-full max-w-[500px] flex-nowrap'
+            ):
+                stats = [
+                    ('All books', book_df["QUANTITY"].sum(axis=0)),
+                    ('Unique books', len(book_df)),
+                    ('Late books', len(late_lends)),
+                ]
+
+                for title, number in stats:
+                    with ui.column().classes(
+                        'flex-1 h-full flex items-center justify-center'
+                    ):
+                        ui.label(title).classes(
+                            'text-xl font-semibold text-gray-600'
+                        )
+                        ui.label(number).classes(
+                            'text-7xl font-extrabold text-gray-800'
+                        )
+
+        # Right half (empty)
+        with ui.column().classes('w-1/2 h-full p-10'):
+            ui.label('Late book lends').classes('text-2xl font-bold mb-4 text-gray-700')
+            
+            ui.table(
+                columns=[
+                    {'name': 'ID', 'label': 'ID', 'field': 'ID', 'align': 'left'},
+                    {'name': 'NAME', 'label': 'NAME', 'field': 'NAME', 'align': 'left'},
+                    {'name': 'CLASS', 'label': 'CLASS', 'field': 'CLASS', 'align': 'left'},
+                    {'name': 'EMAIL', 'label': 'EMAIL', 'field': 'EMAIL', 'align': 'left'},
+                    {'name': 'BOOK_ID', 'label': 'BOOK_ID', 'field': 'BOOK_ID', 'align': 'left'},
+                    {'name': 'END_DATE', 'label': 'END_DATE', 'field': 'END_DATE', 'align': 'left'},
+                    {'name': 'STATUS', 'label': 'STATUS', 'field': 'STATUS', 'align': 'left'},
+                ],
+                rows=late_lends,
+                row_key='title',
+            ).classes('w-full shadow-md rounded-xl')
